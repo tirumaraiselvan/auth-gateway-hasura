2fdbe92331964be6f75d58b97513abc4c24a96d1
Tirumarai Selvan A <tiru@hasura.io>
2020-05-26 15:47:13 +0530

    add gateway logic

--- server.js
+++ server.js
@@ -12,31 +12,43 @@ var port = process.env.PORT || 3000;
    2) Fetches userInfo in a mock function
    3) Return hasura variables
 */
-function fetchUserInfo (token, cb) {
-  // This function takes a token and then makes an async
-  // call to the session-cache or database to fetch
-  // data that is needed for Hasura's access control rules
-  cb();
+function proxyExternalAuth (token, response) {
+    // e.g. you might proxy to a webhook auth service and fwd the response
+    requestClient('https://infinite-tor-17057.herokuapp.com/webhook', function(error, authResp, body) {
+        if (!error) {
+            response.status(authResp.statusCode).json(JSON.parse(body));
+        }
+        else {
+            response.status(500).json({"message": body});
+        }
+    });
 }
-app.get('/', (req, res) => {
-  res.send('Webhooks are running');
-});
 
-app.get('/webhook', (request, response) => {
-  // Extract token from request
-  var token = request.get('Authorization');
-
-  // Fetch user_id that is associated with this token
-  fetchUserInfo(token, (result) => {
-
-    // Return appropriate response to Hasura
+function proxyInternalAuth (token, response) {
+    // e.g. you might proxy to a jwt service or verify jwt token here
     var hasuraVariables = {
-      'X-Hasura-Role': 'user',  // result.role
-      'X-Hasura-User-Id': '1'    // result.user_id
+        'X-Hasura-Role': 'internal',  // result.role
+        'X-Hasura-User-Id': '1'    // result.user_id
     };
     response.json(hasuraVariables);
-  });
-});
+}
+
+app.get('/', (request, response) => {
+  // Extract token from request
+    var token = request.get('Authorization');
+    var appName = request.get('X-Solaire-App');
+    switch(appName) {
+    case "external":
+        proxyExternalAuth(token, response);
+        break;
+    case "internal":
+        proxyInternalAuth(token, response);
+        break;
+    default:
+        response.status(400).json({"message": "x-solaire-app header not found"});
+    }
+  }
+);
 
 // listen for requests :)
 var listener = app.listen(port, function () {
